104e89a44a1901bf888861cdb871ac08f8164353
nairadi <nairadi94@gmail.com>
2019-09-20 01:59:02 -0400

    Initial Commit

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Instance/ member variable
+    [SerializeField] private Text storyTextComponent;
+    [SerializeField] private State startingState;
+
+    State state;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        storyTextComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextState();
+
+        for(int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    state = nextStates[0];
+        //} else if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    state = nextStates[1];
+        //} else if (Input.GetKeyDown(KeyCode.Alpha3))
+        //{
+        //    state = nextStates[2];
+        //}
+        storyTextComponent.text = state.GetStateStory();
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)] [SerializeField] string storyText; // Text area 1st number shows min size in the inspector or our field and 2nd number of lines after which it will add a scroll bar
+    [SerializeField] private State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+
+
+    public State[] GetNextState()
+    {
+        return nextStates;
+        //return nextStates[(currStateIndex + 1) % nextStates.Length];
+    }
+}
